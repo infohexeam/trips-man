137cc8c1df26f26f89231e3c02bf9c7db4f28e06
Rashid KC <rashid.kc@hexeam.in>
2023-06-06 14:35:34 +0530

    profile Image

--- TripsMan/Modules/Main/Controller/Profile/ProfileViewController.swift
+++ TripsMan/Modules/Main/Controller/Profile/ProfileViewController.swift
@@ -333,6 +333,7 @@ extension ProfileViewController {
             self.hideIndicator()
             if response.error == nil {
                 if response.value!.status == 1 {
+                    print("responseMessage: \(response.value!.message)")
                     self.selectedImageURL = response.value!.data.url
                 } else {
                     self.view.makeToast(response.value!.message)
@@ -552,12 +553,53 @@ extension ProfileViewController: ImagePickerDelegate {
         print("selected \(imageName)")
         if let image = image {
             print("image picked \(imageName)")
-            let imgData = image.jpegData(compressionQuality: 0.2)!
+            if let imgData = image.heic() {
+                print("--- imgData found")
+                uploadImage(imgData: imgData)
+
+            } else if let imgData = image.jpegData(compressionQuality: 0.2) {
+                print("-------picked")
+                uploadImage(imgData: imgData)
+
+            }
 //            profileImage.image = image
-            print("-------picked")
-            uploadImage(imgData: imgData)
         }
     }
+}
+
+extension UIImage {
+    var heic: Data? { heic() }
+    func heic(compressionQuality: CGFloat = 1) -> Data? {
+        guard
+            let mutableData = CFDataCreateMutable(nil, 0),
+            let destination = CGImageDestinationCreateWithData(mutableData, "public.heic" as CFString, 1, nil),
+            let cgImage = cgImage
+        else { return nil }
+        CGImageDestinationAddImage(destination, cgImage, [kCGImageDestinationLossyCompressionQuality: compressionQuality, kCGImagePropertyOrientation: cgImageOrientation.rawValue] as CFDictionary)
+        guard CGImageDestinationFinalize(destination) else { return nil }
+        return mutableData as Data
+    }
+}
+
+extension CGImagePropertyOrientation {
+    init(_ uiOrientation: UIImage.Orientation) {
+        switch uiOrientation {
+            case .up: self = .up
+            case .upMirrored: self = .upMirrored
+            case .down: self = .down
+            case .downMirrored: self = .downMirrored
+            case .left: self = .left
+            case .leftMirrored: self = .leftMirrored
+            case .right: self = .right
+            case .rightMirrored: self = .rightMirrored
+        @unknown default:
+            fatalError()
+        }
+    }
+}
+
+extension UIImage {
+    var cgImageOrientation: CGImagePropertyOrientation { .init(imageOrientation) }
 }
 
 
