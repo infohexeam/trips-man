b9edd68b3f8060a7aa223fd49db2376d896b8d1b
Rashid KC <rashid.kc@hexeam.in>
2023-07-24 09:28:46 +0530

    localisation - constants

--- TripsMan/Resources/Constants/LocalizationStrings.swift
+++ TripsMan/Resources/Constants/LocalizationStrings.swift
@@ -54,10 +54,16 @@ struct L {
     }
     
     static func paymentWaitingMessage(for module: String, with bookingNo: String) -> String {
-        return "Your \(K.getModuleText(of: module)) booking has been successfully confirmed. And payment verification is in process. If you have any questions or need assistance, contact our admin at \(K.adminContactNo) or \(K.adminContactEmail) Booking No: \(bookingNo)"
+        if SessionManager.shared.getLanguage().code == "ar" {
+            return "تم تأكيد حجزك رقم \(K.getModuleText(of: module)) بنجاح. والتحقق من الدفع قيد المعالجة. إذا كانت لديك أي أسئلة أو كنت بحاجة إلى المساعدة ، فاتصل بالمسؤول لدينا على الرقم \(K.adminContactNo) أو \(K.adminContactEmail).\nرقم الحجز: \(bookingNo)"
+        }
+        return "Your \(K.getModuleText(of: module)) booking has been successfully confirmed. And payment verification is in process. If you have any questions or need assistance, contact our admin at \(K.adminContactNo) or \(K.adminContactEmail).\nBooking No: \(bookingNo)"
     }
     
     static func bookingFailedMessage() -> String {
+        if SessionManager.shared.getLanguage().code == "ar" {
+            return "نعتذر ، ولكن يبدو أن هناك مشكلة في حجزك. للأسف ، لا يمكن تأكيد الحجز في هذا الوقت. نوصي بالاتصال بفريق الدعم للحصول على مزيد من المساعدة. ونحن نعتذر عن أي إزعاج. اتصل بنا على \(K.adminContactNo) أو \(K.adminContactEmail)"
+        }
         return "We apologise, but there seems to be an issue with your booking. Unfortunately, the booking could not be confirmed at this time. We recommend contacting our support team for further assistance. We apologize for any inconvenience caused. Contact us at \(K.adminContactNo) or \(K.adminContactEmail)"
     }
 }
